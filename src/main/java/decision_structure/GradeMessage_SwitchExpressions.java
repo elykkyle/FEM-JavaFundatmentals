bdc69de94aba3569545565f21a6e79ce80af74a6
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-03-14 11:30:44 -0500

    basics, decision structures and loops

---
+++ src/main/java/decision_structure/GradeMessage_SwitchExpressions.java
@@ -0,0 +1,25 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class GradeMessage_SwitchExpressions {
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+
+        String grade = scanner.next();
+        scanner.close();
+
+        String message = switch (grade){
+            case "A", "B" -> "Excellent job!";
+//            case "B" -> "Great job!";
+            case "C" -> {
+                yield "Good job!";
+            }
+            case "D" -> "You need to work a bit harder.";
+            case "F" -> "Uh oh!";
+            default -> "Error: invalid grade.";
+        };
+        System.out.println(message);
+    }
+}
