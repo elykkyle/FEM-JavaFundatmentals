f6c9ce1d5b26f122eeed8056ea118f866bcba48f
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-03-15 17:02:20 -0500

    exception handling

---
+++ src/main/java/exceptions/MultipleExceptionHandling.java
@@ -0,0 +1,34 @@
+package exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MultipleExceptionHandling {
+
+    public static void main(String[] args) {
+        File file = new File("files/numbers.txt");
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file);
+            while (fileReader.hasNext()) {
+                System.out.println(fileReader.nextDouble());
+            }
+        }
+        catch (FileNotFoundException | InputMismatchException e) {
+            System.out.println(e.getMessage());
+        }
+//        catch (InputMismatchException e) {
+//            System.out.println(e.printStackTrace());
+//        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+//        finally {
+//            fileReader.close();
+//            System.out.println("fileReader closed.");
+//        }
+
+    }
+}
